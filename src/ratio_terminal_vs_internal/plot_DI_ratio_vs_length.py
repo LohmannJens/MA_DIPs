db134d69d0fe78943f43f17797d3756405f1d8c7
Jens Lohmann <jens.johann@outlook.de>
2022-05-05 09:21:17 +0200

    Final regression analysis for external/internal vs length

--- src/ratio_terminal_vs_internal/plot_DI_ratio_vs_length.py
+++ src/ratio_terminal_vs_internal/plot_DI_ratio_vs_length.py
@@ -12,27 +12,34 @@ df = pd.read_excel(xlsx_path, skiprows=[9,10,11,12])
 x = np.array(df["Length"]).reshape((-1, 1))
 y = np.array(df["average"])
 err = np.array(df["standard_deviation"])
+segments = np.array(df["Reference"])
+segments[5] = "NA"
 
 fig, ax = plt.subplots(figsize = (9, 9))
 ax.scatter(x, y, s=60, alpha=0.7, edgecolors="k")
 ax.errorbar(x, y, yerr=err, fmt="o", capsize=5.0)
 
-#model = LinearRegression().fit(x, y)
+for i, seg in enumerate(segments):
+    ax.annotate(seg, (x[i], y[i]))
+
+model = LinearRegression().fit(x, y)
 zero_model = LinearRegression(fit_intercept=False).fit(x, y)
 
 model_x = np.linspace(0, 2341, num=100).reshape((-1, 1))
 
-#ax.plot(x_model, model.predict(x_model), label=f"{model.score(x, y):.4f}")
-ax.plot(model_x, zero_model.predict(model_x), label="linear model starting at 0")
+ax.plot(x, model.predict(x), label=f"linear model 1 (R²: {model.score(x, y):.2f})")
+ax.plot(model_x, zero_model.predict(model_x), label=f"linear model 2 (R²: {zero_model.score(x, y):.2f})")
 
 exp_x = x.reshape((1, -1))[0]
 exp_model_x = model_x.reshape((1, -1))[0]
 exp_model = np.polyfit(exp_x, np.log(y), 1)
 ax.plot(exp_model_x, np.exp(exp_model[1]) * np.exp(exp_model[0] * exp_model_x), label="exponential model")
 
-
+ax.set_xlabel("sequence position")
+ax.set_ylabel("ratio terminal/internal")
 ax.set_xlim(left=0)
 plt.legend(loc="upper left")
 
 
-plt.show()
+fig_path = os.path.join("results", "regression_internal_vs_external.pdf")
+plt.savefig(fig_path)
