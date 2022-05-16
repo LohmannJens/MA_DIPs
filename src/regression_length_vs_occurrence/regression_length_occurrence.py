51dd320665cf5c3112ae336df6ef0a72629c5971
Jens Lohmann <jens.johann@outlook.de>
2022-05-16 15:04:49 +0200

    refactoring regression analysis

--- src/regression_length_vs_occurrence/regression_length_occurrence.py
+++ src/regression_length_vs_occurrence/regression_length_occurrence.py
@@ -37,7 +37,7 @@ def format_dataset_for_plotting(df, dataset_name: str)-> (list, list, list):
     if dataset_name == "schwartz":
         x = df["Length"]
         y = df["average"]
-        err = np.array(df["standard_deviation"])
+        err = np.array(df["standard_deviation"]) / np.array(y).sum()
     else:
         x = list()
         y = list()
@@ -45,12 +45,82 @@ def format_dataset_for_plotting(df, dataset_name: str)-> (list, list, list):
         for s in SEGMENTS:
             y.append(df.loc[df["Segment"] == s]["NGS_read_count"].sum())
             x.append(get_seq_len(dataset_name, s))
-    return np.array(x), np.array(y) / np.array(y).sum(), err / np.array(y).sum()
+    return np.array(x), np.array(y) / np.array(y).sum(), err
+
+
+def fit_models_and_plot_data(x: list, y: list, y_exp: list, err: list, k: str)-> None:
+    '''
+        Creates the linear and exponential model for the given data and plots
+        the results.
+        :param x: data for x axis (segment length)
+        :param y: data for y axis (DI occurrence) as relative values 
+        :param y_exp: cleaned y values for the exponential model (no 0 values)
+        :param err: data for the error bar (only for schwartz dataset)
+        :param k: name of the strain/data set
+
+        :return: None
+    '''
+    def label_scatter(x, y, k):   
+        for i, s in enumerate(SEGMENTS):
+            ax.annotate(s, (x[i], y[i]))
+            if k == "IVA":
+                ax.annotate(s, (x[i+8], y[i+8]))
+                ax.annotate(s, (x[i+16], y[i+16]))
+
+    x_plot = np.linspace(0, 2341, num=100).reshape((-1, 1))
+    fig, ax = plt.subplots(1, 1, figsize=(10, 10), tight_layout=True)
+ 
+    ax.scatter(x, y, label="observation")
+    label_scatter(x, y, k)
+    if k == "schwartz":
+        ax.errorbar(x, y, yerr=err, fmt="o", capsize=5.0)
+
+    # include expected values (perfect correlation DI count and length)
+    if k == "IVA":
+        y_expected = x.copy().astype("float64")
+        y_expected[0:8] = y_expected[0:8] / y_expected[0:8].sum()
+        y_expected[8:16] = y_expected[8:16] / y_expected[8:16].sum()
+        y_expected[16:24] = y_expected[16:24] / y_expected[16:24].sum()
+    else:
+        y_expected = x / x.sum()
+
+    ax.scatter(x, y_expected, label="expected", color="grey")
+    label_scatter(x, y_expected, k)
+
+    # fit the models
+    model = LinearRegression().fit(x.reshape((-1, 1)), y)
+    exp_model = np.polyfit(x, np.log(y_exp), 1)
+
+    # predict values using the fitted models
+    y_pred = model.predict(x.reshape((-1, 1)))
+    y_exp_pred = np.exp(exp_model[1]) * np.exp(exp_model[0] * x_plot)
+
+    # plotting the results
+    score = model.score(x.reshape((-1, 1)), y)
+    ax.plot(x, y_pred, label=f"linear model (R²: {score:.2f})", color="green")
+    ax.plot(x_plot, y_exp_pred, label="exponential model", color="orange")
+
+    # mark x axis intersection
+    ax.plot((-model.intercept_/model.coef_), 0, 'ro')
+    ax.annotate(f"{(-model.intercept_/model.coef_)[0]:.2f}", ((-model.intercept_/model.coef_), 0))
+
+    # set labels and title
+    ax.legend(loc="upper left")
+    ax.set_title(f"{k}")
+    ax.set_xlim(left=0)
+    ax.set_ylim(bottom=0)
+    ax.set_xlabel("sequence position")
+    ax.set_ylabel("relative DI occurrence")
+
+    # save final figure
+    save_path = os.path.join(RESULTSPATH, "regression_length_count", f"{k}_regression_analysis.pdf")
+    plt.savefig(save_path)
+    plt.close()
 
 
 def perform_regression_analysis(data: dict)-> None:
     '''
-        loops over all datasets. Creates a linear and an exponential model for
+        Loops over all datasets. Creates a linear and an exponential model for
         all and plots these. Also creates one for all three Influenza strains
         together.
         :param data: dictionary with name as key and data frame as value
@@ -58,14 +128,7 @@ def perform_regression_analysis(data: dict)-> None:
         :return: None
     '''
     for k, v in data.items():
-        fig, ax = plt.subplots(1, 1, figsize=(10, 10), tight_layout=True)
         x, y, err = format_dataset_for_plotting(v, k)
-
-        ax.scatter(x, y, label="observation")
-        if err.size != 0:
-            ax.errorbar(x, y, yerr=err, fmt="o", capsize=5.0)
-        for i, s in enumerate(SEGMENTS):
-            ax.annotate(s, (x[i], y[i]))
 
         # clean data for exponential model (y == 0 is invalid)
         y_exp = y
@@ -74,7 +137,8 @@ def perform_regression_analysis(data: dict)-> None:
             if y_exp[idx] == 0:
                 y_exp[idx] = 0.000001
             idx += 1
-        
+
+        # collect data for the model over three IV A strains
         if k in ["Cal07", "NC", "Perth"]:
             if "x_IVA" in locals():
                 x_IVA = np.concatenate((x_IVA, x))
@@ -87,61 +151,10 @@ def perform_regression_analysis(data: dict)-> None:
                 y_IVA_exp = y_exp
                 y_IVA_expected = x / x.sum()
 
-        x_plot = np.linspace(0, 2341, num=100).reshape((-1, 1))
-
-        model = LinearRegression().fit(x.reshape((-1, 1)), y)
-        exp_model = np.polyfit(x, np.log(y_exp), 1)
-
-        y_pred = model.predict(x.reshape((-1, 1)))
-        y_exp_pred = np.exp(exp_model[1]) * np.exp(exp_model[0] * x_plot)
-
-        ax.plot(x, y_pred, label=f"linear model (R²: {model.score(x.reshape((-1, 1)), y):.2f})")
-        ax.plot(x_plot, y_exp_pred, label="exponential model")
-
-        ax.scatter(x, x / x.sum(), label="expected", color="grey")
-
-        ax.legend(loc="upper left")
-        ax.set_title(f"{k}")
-        ax.set_xlim(left=0)
-        ax.set_ylim(bottom=0)
-        ax.set_xlabel("sequence position")
-        ax.plot((-model.intercept_/model.coef_), 0, 'ro')
-        ax.annotate(f"{(-model.intercept_/model.coef_)[0]:.2f}", ((-model.intercept_/model.coef_), 0))
-
-        save_path = os.path.join(RESULTSPATH, "regression_length_count", f"{k}_regression_analysis.pdf")
-        plt.savefig(save_path)
-        plt.close()
+        fit_models_and_plot_data(x, y, y_exp, err, k)
 
     # do regression for all three IV A strains together
-    fig, ax = plt.subplots(1, 1, figsize=(10, 10), tight_layout=True)
-    ax.scatter(x_IVA, y_IVA, label="observation")
-
-    model = LinearRegression().fit(x_IVA.reshape((-1, 1)), y_IVA)
-    exp_model = np.polyfit(x_IVA, np.log(y_IVA_exp), 1)
-
-    y_pred = model.predict(x_IVA.reshape((-1, 1)))
-    y_exp_pred = np.exp(exp_model[1]) * np.exp(exp_model[0] * x_plot)
-
-    ax.plot(x_IVA, y_pred, label=f"linear model (R²: {model.score(x_IVA.reshape((-1, 1)), y_IVA):.2f})")
-    ax.plot(x_plot,  y_exp_pred, label="exponential model")
-
-    ax.scatter(x_IVA, y_IVA_expected, label="expected", color="grey")
-
-    for i, s in enumerate(SEGMENTS):
-        ax.annotate(s, (x_IVA[i], y_IVA[i]))
-        ax.annotate(s, (x_IVA[i+8], y_IVA[i+8]))
-        ax.annotate(s, (x_IVA[i+16], y_IVA[i+16]))
-    ax.legend(loc="upper left")
-    ax.set_title(f"All three IV A strains")
-    ax.set_xlim(left=0)
-    ax.set_ylim(bottom=0)
-    ax.set_xlabel("sequence position")
-    ax.plot((-model.intercept_/model.coef_), 0, 'ro')
-    ax.annotate(f"{(-model.intercept_/model.coef_)[0]:.2f}", ((-model.intercept_/model.coef_), 0))
-
-    save_path = os.path.join(RESULTSPATH, "regression_length_count", f"IVA_regression_analysis.pdf")
-    plt.savefig(save_path)
-    plt.close()
+    fit_models_and_plot_data(x_IVA, y_IVA, y_IVA_exp, err, "IVA")
 
 
 if __name__ == "__main__":
