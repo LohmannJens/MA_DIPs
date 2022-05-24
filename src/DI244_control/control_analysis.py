9e5104433d33cfe09aea30d8be0ac07282c679be
Jens Lohmann <jens.johann@outlook.de>
2022-05-24 15:54:15 +0200

    first draft of testing the results with DI244 sample

---
+++ src/DI244_control/control_analysis.py
@@ -0,0 +1,59 @@
+"""
+
+"""
+import os
+import sys
+
+from Bio import SeqIO
+
+sys.path.insert(0, "..")
+from utils import DATAPATH, RESULTSPATH, SEGMENTS, load_excel, load_short_reads, get_sequence
+sys.path.insert(0, "../density_and_length_analysis")
+from composition_junction_site import count_nucleotide_occurrence, calculate_overlapping_nucleotides
+
+START = 244
+END = 2191
+LENGTH = 2341
+
+
+def load_sequence_as_dict(path: str)-> object:
+    '''
+
+    '''
+    record = SeqIO.read(path, "fasta")
+    dna_seq = record.seq
+    rna_seq = dna_seq.transcribe()
+
+    data = dict({"ID": record.id,
+                 "Length": LENGTH,
+                 "Start": START,
+                 "End": END,
+                 "WholeSequence": rna_seq,
+                 "DelSequence": rna_seq[:START] + rna_seq[END-1:]})
+
+    return data
+
+
+if __name__ == "__main__":
+    # load sequence
+    # save as dict
+    fasta_file = os.path.join(DATAPATH, "Dimmock2008", "PB2.fasta")
+    data = load_sequence_as_dict(fasta_file)
+
+    # count nucleotides before and after junction site with
+    start = count_nucleotide_occurrence(data["WholeSequence"], data["Start"])
+    end = count_nucleotide_occurrence(data["WholeSequence"], data["End"])
+
+    print(start, end)
+    print(data["WholeSequence"][START-5:START+5])
+    print(data["WholeSequence"][END-5:END+5])
+
+    # calculate number of overlapping nucleotides
+    # gives numver of overlapping nt and sequence of overlap
+    count, overlap_seq = calculate_overlapping_nucleotides(data["WholeSequence"], data["Start"], data["End"])
+
+    print(count, overlap_seq)
+
+
+
+
