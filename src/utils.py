41eaf5a4297f64bcbdfa320b2a309132f96193f3
Jens Lohmann <jens.johann@outlook.de>
2022-06-28 15:48:50 +0200

    Modified loading of pelz dataset

--- src/utils.py
+++ src/utils.py
@@ -144,15 +144,15 @@ def get_stat_symbol(p: float)-> str:
 def load_pelz_dataset()-> object:
     '''
         Loads the data from Pelz et al 2019 publication.
-        Returns a dictionary with eight key, value pairs one for each segment.
-        The data is stored in a data frame with columns: 'DIs', 'overall_count'
+        Is structured the same way as data from alnaji 2019.
 
-        :return: dictionary with 'segment': 'DataFrame' pairs
+        :return: dictionary with one key, value pair
     '''
     file_path = os.path.join(DATAPATH, "Pelz2021", "ShortDeletions_AbsoluteValues.xlsx")
     data_dict = pd.read_excel(io=file_path,
                               sheet_name=None,
                               header=0,
                               na_values=["", "None"])
+
     return data_dict
 
