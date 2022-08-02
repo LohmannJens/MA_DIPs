c96edd77e842af9e5db0aa342af70a2bf5dcb89f
Jens Lohmann <jens.johann@outlook.de>
2022-08-02 21:06:30 +0200

    Added function to compare Lee and Le Sage data sources of NP density

--- src/density_and_length_analysis/NP_density_analysis.py
+++ src/density_and_length_analysis/NP_density_analysis.py
@@ -24,9 +24,10 @@ from utils import DATAPATH, RESULTSPATH, SEGMENTS, QUANT, S_ROUNDS
 from utils import load_alnaji_excel, load_short_reads, get_sequence, get_seq_len, get_stat_symbol, generate_sampling_data
 
 
-def load_density_data(path: str)-> dict:
+def load_Lee_density_data(path: str)-> dict:
     '''
-        Loads the density data for all eight segments.
+        Loads the density data for all eight segments. Uses the Lee et al. 2017
+        paper as source.
         :param path: path to the location where the csv files are stored
 
         :return: dictionary with segment name as key and data frame as value
@@ -38,9 +39,10 @@ def load_density_data(path: str)-> dict:
     return density_dict
 
 
-def load_my_density_data(path: str)-> dict:
+def load_Sage_density_data(path: str)-> dict:
     '''
-        Loads the density data for all eight segments.
+        Loads the density data for all eight segments. Uses the Le Sage et al.
+        2019 paper as source.
         :param path: path to the location where the tsv files are stored
 
         :return: dictionary with segment name as key and data frame as value
@@ -138,6 +140,32 @@ def map_positions_to_density(data: dict, density_data: dict)-> dict:
     return NGS_dict
 
 
+def map_dens_to_dens(strain: str, dens_1: dict, dens_2: dict)-> None:
+    '''
+        Plots the NP density of two different papers as source together.
+        :param data: dict with information about start and end position
+        :param dens_1: dict with dens data (key is segment name) from Lee
+        :param dens_2: dict with dens data (key is segment name) from Le Sage
+
+        :return: None
+    '''
+    for k, v in dens_1.items():
+        fig, axs = plt.subplots(8, 1, figsize=(7, 14), tight_layout=True)
+        for i, s in enumerate(SEGMENTS):
+            axs[i].plot(dens_1[s]["x"], dens_1[s]["y"], label="Lee", alpha=0.5, color="green", fillstyle="full")
+            axs[i].plot(dens_2[s]["x"], dens_2[s]["y"], label="Le Sage", alpha=0.5, color="blue", fillstyle="full")
+
+            axs[i].set_title(f"{s}")
+            axs[i].set_xlim(left=0, right=max(dens_1[s]["x"]))
+            axs[i].set_ylim(bottom=0, top=105)
+            axs[i].set_xlabel("sequence position")
+            axs[i].set_ylabel("high/low NP area")
+
+        save_path = os.path.join(RESULTSPATH, "NP_density", f"{strain}_map_density_data_sources.pdf")
+        plt.savefig(save_path)
+        plt.close()
+
+
 def compare_position_with_density(data: dict, density_data: dict, all_reads: dict)-> None:
     '''
         Checks how many of the junction sites are at a position of low NP
@@ -214,7 +242,8 @@ if __name__ == "__main__":
     # Plotting NP data (from Sage 2019) against junction sites
     #    Cal07 data from Alnaji 2019
     Cal07_dens_path = os.path.join(density_path, "Cal07")
-    Cal07_dens_data = load_my_density_data(Cal07_dens_path)
+    Cal07_dens_data = load_Sage_density_data(Cal07_dens_path)
+    map_dens_to_dens("Cal07", load_Lee_density_data(Cal07_dens_path), Cal07_dens_data)
     NGS_count_dict = map_positions_to_density(all_reads_dict, Cal07_dens_data)
     compare_position_with_density(NGS_count_dict, Cal07_dens_data, all_reads_dict)
         
@@ -222,7 +251,9 @@ if __name__ == "__main__":
     WSN_count_file = os.path.join(DATAPATH, "Boussier2020", "Supplemental_Table_S2.xlsx")
     WSN_reads_dict = load_WSN_data(WSN_count_file)
     WSN_dens_path = os.path.join(density_path, "WSN")
-    WSN_dens_data = load_my_density_data(WSN_dens_path)
+    WSN_dens_data = load_Sage_density_data(WSN_dens_path)
+    map_dens_to_dens("WSN", load_Lee_density_data(WSN_dens_path), WSN_dens_data)
     WSN_NGS_count_dict = map_positions_to_density(WSN_reads_dict, WSN_dens_data)
     compare_position_with_density(WSN_NGS_count_dict, WSN_dens_data, WSN_reads_dict)
     
+
