b9ee4c680b6ebde76e58d8fad01ffd2726648d3e
Jens Lohmann <jens.johann@outlook.de>
2022-10-27 10:16:08 +0200

    segment distribution analysis & lin reg for Alnaji 2021 14 hpi inter <-> extra

--- src/intra_vs_extracellular/14hpi_alnaji_analysis.py
+++ src/intra_vs_extracellular/14hpi_alnaji_analysis.py
@@ -11,8 +11,10 @@ import matplotlib.pyplot as plt
 from matplotlib_venn import venn2
 
 sys.path.insert(0, "..")
+sys.path.insert(0, "../di_rna_conservation")
 from utils import DATAPATH, RESULTSPATH
 from utils import load_hpi14_alnaji
+from alnaji_replicates_compared import analyze_segment_distribution, linear_regression_analysis
 
 
 def venn_analysis(data: dict)-> None:
@@ -42,3 +44,22 @@ if __name__ == "__main__":
     data_dict = load_hpi14_alnaji()
 
     venn_analysis(data_dict)
+
+    analyze_segment_distribution(data_dict["PR8"], "Class", ["all", "internal", "external"])
+    src = os.path.join(RESULTSPATH, "di_rna_conservation", "segment_distribution.png")
+    dst = os.path.join(RESULTSPATH, "intra_vs_extracellular", "segment_distribution.png")
+    os.rename(src, dst)
+
+    # Linear regression analysis
+    strain = "PR8"
+    df = data_dict["PR8"]
+    inter_df = df.loc[df["Class"] == "internal"]
+    extra_df = df.loc[df["Class"] == "external"]
+
+    dst = os.path.join(RESULTSPATH, "intra_vs_extracellular", f"PR8_regression_analysis_full.pdf")
+    linear_regression_analysis(strain, df, dst)
+    dst = os.path.join(RESULTSPATH, "intra_vs_extracellular", f"PR8_regression_analysis_inter.pdf")
+    linear_regression_analysis(strain, inter_df, dst)
+    dst = os.path.join(RESULTSPATH, "intra_vs_extracellular", f"PR8_regression_analysis_extra.pdf")
+    linear_regression_analysis(strain, extra_df, dst)
+
