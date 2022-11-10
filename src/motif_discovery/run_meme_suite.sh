571cd9df306b73119c39f5b24c033c37cb875c1a
Jens Lohmann <jens.johann@outlook.de>
2022-11-10 12:05:09 +0100

    Included info about arguments and added selection of mode when using 'window_##'

--- src/motif_discovery/run_meme_suite.sh
+++ src/motif_discovery/run_meme_suite.sh
@@ -4,17 +4,31 @@ dir="../../data/meme_suite/alnaji2019"
 dir_full="$dir/full_sequences"
 dir_crop="$dir/cropped_sequences"
 
-#dataset="IVA"
+# Parsing arguments
+# -d dataset -> name of dataset without '.fasta' (e.g. 'all', 'M', 'PB2')
+# -c cropped -> defines format of the input sequences
+    # full sequences where used: 'full'
+    # sequences without deletion site: 'crop' 
+    # only a window of size ##  around the deletion site was used: 'window_##'
+# -m mode    -> mode that was used to create the sequences
+    # only needs to be specified if 'window_##' is used
+# -p program -> program to run (meme, streme, xstreme, glam)
 
-while getopts d:c:p: flag
+while getopts d:c:m:p: flag
 do
     case "${flag}" in
         d) dataset=${OPTARG};; # name of dataset (without .fasta)
-        c) cropped=${OPTARG};; # 'crop', 'full' or 'window'
+        c) cropped=${OPTARG};; # 'crop', 'full' or 'window_##'
+        m) mode=${OPTARG};; # mode to use; needs to be specified if 'window_##' is used
         p) program=${OPTARG};; # program to run (meme, streme, xstreme, glam)
     esac
 done
 
+if ! ([ "$mode" == "" ] || [ "$mode" == "combined" ] || [ "$mode" == "onlyremain" ])
+then
+    echo "when -m is selected 'combined' or 'onlyremain' has to be written after"
+    exit
+fi
 
 if [ $cropped == "full" ]
 then
@@ -24,7 +38,7 @@ then
     path="$dir_crop/$dataset"
 elif [ ${cropped:0:6} == "window" ]
 then
-    path="$dir/$cropped""_sequences_combined/$dataset"
+    path="$dir/$cropped""_sequences_$mode/$dataset"
 elif [ $cropped == "high_ngs" ]
 then
     path="$dir/$cropped/$dataset"
