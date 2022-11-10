60931628816b4a52f4d734aea12b15ae6ce46a1b
Jens Lohmann <jens.johann@outlook.de>
2022-11-10 12:05:43 +0100

    Added 'only_remain' option

--- src/motif_discovery/prepare_sequences_for_meme.py
+++ src/motif_discovery/prepare_sequences_for_meme.py
@@ -107,7 +107,7 @@ def create_cropped_seq_files(d: dict)-> None:
             write_sequence(record, k, seg, root_folder)
 
 
-def create_windows_del_site_files(d: dict, n: int, combine: bool)-> None:
+def create_windows_del_site_files(d: dict, n: int, combine: bool, only_remain: bool)-> None:
     '''
         Creates FASTA files for a n wide window around the start and end of the
         sequences of the different strains and segments. 
@@ -115,12 +115,16 @@ def create_windows_del_site_files(d: dict, n: int, combine: bool)-> None:
         :param n: half window size (only indicating size in one direction)
         :param combine: states if the start and end window should be
                         concatenated or not
+        :param only_remain: states if only the first and fourth part around 
+                            the deletion site should be kept
 
         :return: None
     '''
     fname = f"window_{n}_sequences"
     if combine:
         fname = f"{fname}_combined"
+    elif only_remain:
+        fname = f"{fname}_onlyremain"
     root_folder = os.path.join(DATAPATH, "meme_suite", "alnaji2019", fname)
     if os.path.exists(root_folder):
         if delete_folder(root_folder):
@@ -141,6 +145,12 @@ def create_windows_del_site_files(d: dict, n: int, combine: bool)-> None:
             # the beginning/end of the whole sequence.
             if combine:
                 window_seq = seq[max(s-n, 0):s+n] + seq[e-n:min(e+n, len(seq))]
+                record = SeqRecord(Seq(window_seq), id=f"{k}_{seg}_{s}_start_{e}_end")
+                write_sequence(record, k, seg, root_folder)
+            elif only_remain:
+                print(s, n, e)
+                window_seq = seq[max(s-n, 0):s] + seq[e:min(e+n, len(seq))]
+                print(window_seq)
                 record = SeqRecord(Seq(window_seq), id=f"{k}_{seg}_{s}_start_{e}_end")
                 write_sequence(record, k, seg, root_folder)
             else:
@@ -192,6 +202,6 @@ if __name__ == "__main__":
 
     seq_library = create_sequence_library(all_reads_dict)
  #   create_cropped_seq_files(seq_library)
-    create_windows_del_site_files(seq_library, 50, True)
+    create_windows_del_site_files(seq_library, 50, False, True)
   #  create_high_NGS_dataset(seq_library, 1000)
 
