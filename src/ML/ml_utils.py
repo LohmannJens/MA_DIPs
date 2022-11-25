1307ca52a97368b2cc24b3776d721a7e2907f972
Jens Lohmann <jens.johann@outlook.de>
2022-11-25 12:50:14 +0100

    Merging duplicates when loading datset

--- src/ML/ml_utils.py
+++ src/ML/ml_utils.py
@@ -27,6 +27,11 @@ def load_all_sets()-> object:
         df["NGS_log_norm"] = df["NGS_log"]/max(df["NGS_log"])
 
         return df
+
+    def merge_duplicates(df):
+        df = df.groupby(["Segment", "Start", "End"]).sum(["NGS_read_count"]).reset_index()
+        return df
+
     # load pelz dataset
     df = load_pelz_dataset()["PR8"]
     df["dataset_name"] = "Pelz"
@@ -34,28 +39,32 @@ def load_all_sets()-> object:
 
     # load kupke dataset
     kupke = load_kupke(corrected=True)["PR8"]
-    kupke["dataset_name"] = "Kupke"
     kupke.drop(["DI", "Length", "Infection", "Num_sample", "Correction"], axis=1, inplace=True)
+    kupke = merge_duplicates(kupke)
+    kupke["dataset_name"] = "Kupke"
     kupke = log_and_norm(kupke)
     df = pd.concat([df, kupke])
 
     # load alnaji 2021 dataset
     alnaji2021 = load_full_alnaji2021()
-    alnaji2021["dataset_name"] = "Alnaji2021"
     alnaji2021.drop(["DI", "Replicate", "Timepoint", "Class"], axis=1, inplace=True)
+    alnaji2021 = merge_duplicates(alnaji2021)
+    alnaji2021["dataset_name"] = "Alnaji2021"
     alnaji2021 = log_and_norm(alnaji2021)
     df = pd.concat([df, alnaji2021])
 
     # load four datasets of alnaji 2019
     alnaji2019 = load_short_reads(load_alnaji_excel())
     for k, v in alnaji2019.items():
-        v["dataset_name"] = f"Alnaji2019_{k}"
         v.drop(["Length"], axis=1, inplace=True)
+        v["NGS_read_count"] = v["NGS_read_count"].astype(int)
+        v = merge_duplicates(v)
+        v["dataset_name"] = f"Alnaji2019_{k}"
         v = log_and_norm(v)
         df = pd.concat([df, v])
 
     df.reset_index(inplace=True)
     df.drop(["index"], axis=1, inplace=True)
-    
+
     return df
 
