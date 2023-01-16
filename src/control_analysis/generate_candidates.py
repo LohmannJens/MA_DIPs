24dbfb508bd16f45dd1724e58e53eff264f3df34
Jens Lohmann <jens.johann@outlook.de>
2023-01-16 09:50:56 +0100

    Added script to generate FASTAS of high pot candidates

---
+++ src/control_analysis/generate_candidates.py
@@ -0,0 +1,51 @@
+'''
+    generates FASTA files of the high pot candidates from Pelz and DI244.
+'''
+import os
+import sys
+
+import pandas as pd
+
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+
+sys.path.insert(0, "..")
+from utils import DATAPATH, RESULTSPATH
+from utils import create_sequence_library
+from high_pot_control_analysis import load_DI244, load_top_DI_RNA_pelz
+
+sys.path.insert(0, "../free_energy_estimations")
+from prepare_sequences_for_RNAvienna import write_sequence
+
+
+def generate_fastas(df: pd.DataFrame)-> None:
+    '''
+        Generates an independent FASTA file for each entry in the given Data
+        Frame
+        :param df: data frame including at least a Name and DelSequence
+
+        :return: None
+    '''
+    root_folder = os.path.join(DATAPATH, "FIRM-AVP")
+    print(df["DelSequence"])
+    for i, r in df.iterrows():
+        id = r["Name"]
+        seq = Seq(r["DelSequence"])
+        record = SeqRecord(seq, id)
+        write_sequence(record, id, root_folder)
+
+
+if __name__ == "__main__":
+    fasta_file = os.path.join(DATAPATH, "Dimmock2008", "PB2.fasta")
+
+    pelz_top_df = load_top_DI_RNA_pelz()
+    di244_df = load_DI244()
+
+    pelz_top_df = pelz_top_df.drop("NGS_read_count", axis=1)
+    full_df = pd.concat([pelz_top_df, di244_df], ignore_index=True)
+    name_col = full_df.pop("Name")
+    full_df.insert(0, "Name", name_col)
+    seq_df = create_sequence_library({"PR8": full_df})
+
+    generate_fastas(seq_df["PR8"])
+
