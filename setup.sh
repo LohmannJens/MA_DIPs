1620a5f70273fe32868ec4066f8a37f103bd94e6
LohmannJens <jens.johann@outlook.de>
2023-07-03 16:10:53 +0200

    Added subfolders in results folder in setup script

--- setup.sh
+++ setup.sh
@@ -8,5 +8,7 @@ cat << EOF > .config.json
 EOF
 
 mkdir -p data results
+cd results
+mkdir -p control_analysis deletion_length_and_position direct_repeats free_energy_estimations general_validation ML motif_discovery NP_density PR8 regression_length_vs_occurrence relative_occurrence_nucleotides
 
 echo setup complete!
