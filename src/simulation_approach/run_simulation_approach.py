8eb77372d74fc02fa632d2aeb2c34a7cb76c6f51
LohmannJens <jens.johann@outlook.de>
2023-07-12 15:09:49 +0200

    Added de novo loss events and max NGS count

--- src/simulation_approach/run_simulation_approach.py
+++ src/simulation_approach/run_simulation_approach.py
@@ -25,54 +25,56 @@ class DIPClass():
     def get_size(self):
         return len(self.candidate_counts)
 
-    def get_overall_count(self):
+    def get_count(self):
         return np.sum(self.candidate_counts)
-        return self.candidate_counts
 
 class DIPPopulationSimulator():
-    def __init__(self, classes, n_max):
+    def __init__(self, classes, n_max, cand_max):
         self.classes = classes
         self.n_max = n_max
-        self.n = self.get_population_size()
+        self.cand_max = cand_max
         self.timeseries = dict({c.name : list() for c in self.classes})
     
     def get_population_size(self):
         return np.sum([c.get_size() for c in self.classes])
+    
+    def get_population_count(self):
+        return np.sum([c.get_count() for c in self.classes])
 
     def run_simulation(self, steps):
         for i in range(steps):
-    #        print(f"### iteration {i+1} ###")
-            for c in self.classes:
+            loss_event_p = 0.1
+            loss_event = True if loss_event_p > np.random.random() else False
+
+            for cls in self.classes:
                 new_counts = list()
-                for cand in c.candidate_counts:
+                for cand in cls.candidate_counts:
                     thresh = np.random.random()
 
-                    if c.p > thresh:
-                        change = round(cand * (1 - self.n / self.n_max) * c.r_incr)
+                    p = 0.1 if loss_event else cls.p
+
+                    if p > thresh:
+                        change = round(cand * (1 - self.get_population_count() / self.n_max) * cls.r_incr )#* 0.2) # here I have an extra artificial factor included right now
                     else:
-                        change = round(cand * (self.n / self.n_max) * c.r_decr)
+                        change = round(cand * (self.get_population_count() / self.n_max) * cls.r_decr)
 
                     if change <= 0:
                         change = min(change, -3)
                     elif change > 0:
-                        change = min(change, 10000)
+                        change = min(change, 100)
 
                     cand = cand + change
                     if cand > 0:
                         new_counts.append(cand)
 
-                for _ in range(self.n_max - self.n):
+                for _ in range(self.cand_max - self.get_population_size()):
                     de_novo_event = np.random.random()
-                    if de_novo_event < c.de_novo_event_prob:
+                    if de_novo_event < cls.de_novo_event_prob:
                         new_counts.append(np.random.randint(1, 20))
 
-                c.candidate_counts = new_counts    
-                self.timeseries[c.name].append(c.get_size())                
-
-           #     print(f"\t{c.name}:\t{c.get_size()}")
-            #    print(c.get_overall_count())
-
-            self.n = self.get_population_size()
+                cls.candidate_counts = new_counts    
+              #  self.timeseries[cls.name].append(cls.get_size())
+                self.timeseries[cls.name].append(cls.get_count())
 
     def show_results(self):
         fig, axs = plt.subplots(1,1)
@@ -87,7 +89,7 @@ class DIPPopulationSimulator():
 
 
 if __name__ == "__main__":
-    steps = 1000
+    steps = 200
     use_abs = True
     if use_abs:
         Gain = DIPClass("gain", 261, 0.5012221023325012, 1.7862913959727234, -0.49710422318156694, 0.08333851479648849)
@@ -96,7 +98,7 @@ if __name__ == "__main__":
         Gain = DIPClass("gain", 261, 0.5225600824272179, 1.3383349229859336, -0.41407475207207134, 0.08449292411478025)
         Loss = DIPClass("loss", 373, 0.4782325094182225, 1.3973153622239822, -0.5737954358770873, 0.07461418881440231)
 
-    Simulator = DIPPopulationSimulator([Gain, Loss], 1400)
+    Simulator = DIPPopulationSimulator([Gain, Loss], 100000, 1400)
     Simulator.run_simulation(steps)
     Simulator.show_results()
 
