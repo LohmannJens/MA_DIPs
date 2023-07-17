a0aa4b77d7eff4e6d39a5db00d451cd430bc35e0
LohmannJens <jens.johann@outlook.de>
2023-07-17 10:51:27 +0200

    new output and DI RNA count as limiting factor

--- src/simulation_approach/run_simulation_approach.py
+++ src/simulation_approach/run_simulation_approach.py
@@ -22,83 +22,108 @@ class DIPClass():
         self.de_novo_event_prob = de_novo_event_prob
         self.candidate_counts = np.array([np.random.randint(1, 20) for _ in range(self.n)])
 
-    def get_size(self):
+    def get_n_cand(self):
         return len(self.candidate_counts)
 
     def get_count(self):
         return np.sum(self.candidate_counts)
 
 class DIPPopulationSimulator():
-    def __init__(self, classes, n_max, cand_max):
+    def __init__(self, classes, count_max, n_cand_max):
         self.classes = classes
-        self.n_max = n_max
-        self.cand_max = cand_max
-        self.timeseries = dict({c.name : list() for c in self.classes})
+        self.count_max = count_max
+        self.n_cand_max = n_cand_max
+        self.n_cand_timeseries = dict({c.name : list() for c in self.classes})
+        self.count_timeseries = dict({c.name : list() for c in self.classes})
     
-    def get_population_size(self):
-        return np.sum([c.get_size() for c in self.classes])
+    def get_population_n_cand(self):
+        return np.sum([c.get_n_cand() for c in self.classes])
     
     def get_population_count(self):
         return np.sum([c.get_count() for c in self.classes])
 
     def run_simulation(self, steps):
         for i in range(steps):
+            if i % 100 == 0:
+                print(i)
             loss_event_p = 0.1
             loss_event = True if loss_event_p > np.random.random() else False
 
             for cls in self.classes:
                 new_counts = list()
-                for cand in cls.candidate_counts:
+                for cand_counts in cls.candidate_counts:
                     thresh = np.random.random()
 
                     p = 0.1 if loss_event else cls.p
 
                     if p > thresh:
-                        change = round(cand * (1 - self.get_population_count() / self.n_max) * cls.r_incr )#* 0.2) # here I have an extra artificial factor included right now
+                        change = round(cand_counts * (1 - self.get_population_count() / self.count_max) * cls.r_incr * 0.1) # here I have an extra artificial factor included right now
                     else:
-                        change = round(cand * (self.get_population_count() / self.n_max) * cls.r_decr)
+                        change = round(cand_counts * (self.get_population_count() / self.count_max) * cls.r_decr)
 
                     if change <= 0:
                         change = min(change, -3)
-                    elif change > 0:
-                        change = min(change, 100)
 
-                    cand = cand + change
-                    if cand > 0:
-                        new_counts.append(cand)
+                    cand_counts = cand_counts + change
+                    if cand_counts > 0:
+                        new_counts.append(cand_counts)
 
-                for _ in range(self.cand_max - self.get_population_size()):
+                for _ in range(round((self.count_max - self.get_population_count())/50)):
                     de_novo_event = np.random.random()
                     if de_novo_event < cls.de_novo_event_prob:
                         new_counts.append(np.random.randint(1, 20))
 
                 cls.candidate_counts = new_counts    
-              #  self.timeseries[cls.name].append(cls.get_size())
-                self.timeseries[cls.name].append(cls.get_count())
+                self.n_cand_timeseries[cls.name].append(cls.get_n_cand())
+                self.count_timeseries[cls.name].append(cls.get_count())
 
     def show_results(self):
-        fig, axs = plt.subplots(1,1)
-        for k, v in self.timeseries.items():
-            axs.plot(v, label=k)
+        fig, axs = plt.subplots(1,2)
+        for k, v in self.n_cand_timeseries.items():
+            axs[0].plot(v, label=k)
+            axs[0].set_xlabel("time point")
+            axs[0].set_ylabel("number of candidates")
+            axs[0].set_ylim(bottom=0)
+            axs[0].legend()
+        for k, v in self.count_timeseries.items():
+            axs[1].plot(v, label=k)
+            axs[1].set_xlabel("time point")
+            axs[1].set_ylabel("count of candidates")
+            axs[1].set_ylim(bottom=0)
+            axs[1].legend()
 
-        plt.xlabel("time point")
-        plt.ylabel("number of candidates")
-        plt.ylim(bottom=0)
-        plt.legend()
         plt.show()
+
+    def show_candidate_developement(self):
+        fig, axs = plt.subplots(1,2)
+        for k, v in self.n_cand_timeseries.items():
+            axs[0].plot(v, label=k)
+            axs[0].set_xlabel("time point")
+            axs[0].set_ylabel("number of candidates")
+            axs[0].set_ylim(bottom=0)
+            axs[0].legend()
+        for k, v in self.count_timeseries.items():
+            axs[1].plot(v, label=k)
+            axs[1].set_xlabel("time point")
+            axs[1].set_ylabel("count of candidates")
+            axs[1].set_ylim(bottom=0)
+            axs[1].legend()
+
+        plt.show()        
+
 
 
 if __name__ == "__main__":
-    steps = 200
-    use_abs = True
+    steps = 1000
+    use_abs = False
     if use_abs:
-        Gain = DIPClass("gain", 261, 0.5012221023325012, 1.7862913959727234, -0.49710422318156694, 0.08333851479648849)
-        Loss = DIPClass("loss", 373, 0.47635938952884715, 1.7891287030150624, -0.6390361485007808, 0.08504530568538506)
+        Gain = DIPClass("gain", 261, 0.5012221023325012, 1.7862913959727234, -0.49710422318156694, 0.0396850070459469)
+        Loss = DIPClass("loss", 373, 0.47635938952884715, 1.7891287030150624, -0.6390361485007808, 0.044547541073296934)
     else:
-        Gain = DIPClass("gain", 261, 0.5225600824272179, 1.3383349229859336, -0.41407475207207134, 0.08449292411478025)
-        Loss = DIPClass("loss", 373, 0.4782325094182225, 1.3973153622239822, -0.5737954358770873, 0.07461418881440231)
+        Gain = DIPClass("gain", 261, 0.5225600824272179, 1.3383349229859336, -0.41407475207207134, 0.044258198345837274)
+        Loss = DIPClass("loss", 373, 0.4782325094182225, 1.3973153622239822, -0.5737954358770873, 0.039083622712305977)
 
-    Simulator = DIPPopulationSimulator([Gain, Loss], 100000, 1400)
+    Simulator = DIPPopulationSimulator([Gain, Loss], 50000, 1400)
     Simulator.run_simulation(steps)
     Simulator.show_results()
 
