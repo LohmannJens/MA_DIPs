e2ad5797513fdd3dff4ef8b60f96524bd7e73bb4
LohmannJens <jens.johann@outlook.de>
2023-08-17 13:25:20 +0200

    Implemented heatmap for enrichment of common DIs over all datasets

--- src/toolbox_mia/candidate_intersection.py
+++ src/toolbox_mia/candidate_intersection.py
@@ -1,4 +1,5 @@
 
+import os
 import sys
 
 import pandas as pd
@@ -12,6 +13,7 @@ from collections import Counter
 
 sys.path.insert(0, "..")
 from utils import get_sequence, load_full_alnaji2021, load_pelz_dataset, load_pelz_di244_dataset, load_pelz_PB2PB1PA_di244_dataset
+from utils import DATAPATH
 
 sys.path.insert(0, "../direct_repeats")
 from search_direct_repeats import calculate_direct_repeat
@@ -100,18 +102,70 @@ def generate_maximum_overlap_candidates(dfs: list, thresh: int):
     c = grouped.mean()
     
     print(c)
-
-    plt.scatter(counts, dir_reps, alpha=0.05)
-    plt.show()
     '''
+
+
+def plot_heatmap(y,x,vals,ax, format='.2f', cmap='coolwarm', vmin=0, vmax=1,cbar_ax=None, cbar_kws=None):
+    '''Plot heatmap for values in vals, with x (columns) and y (rows) as labels.'''
+    df = pd.DataFrame({'x':x,'y':y,'vals':vals})
+    df = pd.pivot_table(df, index='x', columns='y', values='vals', sort=False)
+    ax = sns.heatmap(df, fmt=format, annot=False, vmin=vmin, vmax=vmax, ax=ax, cbar=True, cmap=cmap, cbar_ax=cbar_ax, cbar_kws=cbar_kws)
+    return ax
+
+
+def generate_overall_heatmap(dfs: list, dfnames: list, thresh: int):
+    '''
+    
+    '''
+    def get_seed_dis():
+        filename = "Seed_pelz_long.xlsx"
+
+        file_path = os.path.join(DATAPATH, "Pelz2021", filename)
+        data_dict = pd.read_excel(io=file_path,
+                                sheet_name=None,
+                                header=0,
+                                na_values=["", "None"],
+                                keep_default_na=False)
+        df = data_dict["PR8"]
+        return df["DI"].tolist() 
+    
+    all_candidates = list()
+    for df in dfs:
+        all_candidates.extend(df["DI"].tolist())
+
+    candidate_counts = Counter(all_candidates)
+
+    elements_to_remove = [element for element, count in candidate_counts.items() if count < thresh]
+    elements_to_remove.extend(get_seed_dis())
+    for element in elements_to_remove:
+        del candidate_counts[element]
+
+    x = list()
+    y = list()
+    vals = list()
+    for df, dfname in zip(dfs, dfnames):
+        NGS_max = max(df["NGS_read_count"])
+        y.extend(candidate_counts.items())
+        x.extend(dfname for _ in range(len(candidate_counts)))
+        for cand, _ in candidate_counts.items():
+            if cand in df["DI"].values:
+                NGS_count = df.loc[df["DI"] == cand, "NGS_read_count"]
+                vals.append(NGS_count/NGS_max)
+            else:
+                vals.append(np.nan)
+
+    fig, ax = plt.subplots(figsize=(10,10))
+    ax = plot_heatmap(y,x,vals,ax, format='.2f', cmap='coolwarm', vmin=0, vmax=1,cbar_ax=None, cbar_kws=None)
+
+    plt.show()
+    exit()
 
 
 if __name__ == "__main__":
     # load all data frames and preprocess with sequence_df(df)
     dfs = list()
     dfnames = list()
-
-    '''
+   
     alnaji_dict = load_full_alnaji2021()
     for k, v in alnaji_dict.items():
         for t in ["3hpi", "6hpi", "14hpi_internal", "14hpi_external", "24hpi"]:
@@ -121,10 +175,9 @@ if __name__ == "__main__":
                 dfs.append(preprocess(k, df))
                 dfnames.append(f"Alnaji2021_{t}_{r}")   
 
-    generate_overlap_matrix_plot(dfs, dfnames)
-    generate_maximum_overlap_candidates(dfs, thresh=10)
-    '''
-
+#    generate_overlap_matrix_plot(dfs, dfnames)
+ #   generate_maximum_overlap_candidates(dfs, thresh=10)
+    
     df_pelz = load_pelz_dataset(long_dirna=True)
     for k, v in df_pelz.items():
         dfs.append(preprocess(k, v))
@@ -132,8 +185,13 @@ if __name__ == "__main__":
 
     df_pelz = load_pelz_dataset(follow_up=True)
     for k, v in df_pelz.items():
-        dfs.append(preprocess(k, v))
-        dfnames.append("Pelz_followup")
+        for col in ["A1", "A2", "A3", "A4", "A5"]:
+            columns = ["Segment", "Start", "End", col]
+            df = v[columns]
+            df[col] = df[col].rename("NGS_read_count")
+            df.rename(columns={col: "NGS_read_count"}, inplace=True)
+            dfs.append(preprocess(k, df))
+            dfnames.append(f"Pelz_followup_{col}")
 
     df_pelz = load_pelz_di244_dataset(by_time=False)
     for k, v in df_pelz.items():
@@ -148,6 +206,7 @@ if __name__ == "__main__":
                 dfs.append(preprocess(k, df))
                 dfnames.append(f"Pelz_Di244_{exp}_{r}")
 
-    generate_overlap_matrix_plot(dfs, dfnames)
-    generate_maximum_overlap_candidates(dfs, thresh=6)
-
+  #  generate_overlap_matrix_plot(dfs, dfnames)
+   # generate_maximum_overlap_candidates(dfs, thresh=6)
+    
+    generate_overall_heatmap(dfs, dfnames, 8)
